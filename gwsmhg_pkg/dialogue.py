dac99d7258332493d4d433b3f7ce34fecdc13138
Peter Williams <pwil3058@gmail.com>
2009-07-09 10:38:40 +1000

    To prevent unfortunate accidents, destroy amodal dialogs on chdir as the alternative of tracking associated repos is proving more trouble than it's worth
    Signed-off-by: Peter Williams <peter_ono@users.sourceforge.net>

--- gwsmhg_pkg/dialogue.py
+++ gwsmhg_pkg/dialogue.py
@@ -16,7 +16,7 @@
 ### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import gtk, os
-from gwsmhg_pkg import cmd_result, icons
+from gwsmhg_pkg import cmd_result, icons, ws_event
 
 main_window = None
 tooltips = gtk.Tooltips()
@@ -62,10 +62,11 @@ class Dialog(gtk.Dialog, BusyIndicator):
     def inform_user(msg, problem_type=gtk.MESSAGE_ERROR):
         inform_user(msg, problem_type, self)
 
-class AmodalDialog(Dialog):
+class AmodalDialog(Dialog, ws_event.Listener):
     def __init__(self, title=None, parent=None, flags=0, buttons=None, rootdir=None):
         flags &= ~gtk.DIALOG_MODAL
         Dialog.__init__(self, title=title, parent=parent, flags=flags, buttons=buttons)
+        ws_event.Listener.__init__(self)
         self.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_NORMAL)
         if rootdir:
             self._rootdir = rootdir
@@ -74,6 +75,9 @@ class AmodalDialog(Dialog):
             self._rootdir = ifce.SCM.get_root()
         from gwsmhg_pkg import utils
         self._rel_rootdir = utils.path_rel_home(self._rootdir)
+        self.add_notification_cb(ws_event.CHANGE_WD, self._change_wd_cb)
+    def _change_wd_cb(self, arg=None):
+        self.destroy()
 
 class MessageDialog(gtk.MessageDialog):
     def __init__(self, parent=None, flags=0, type=gtk.MESSAGE_INFO, buttons=gtk.BUTTONS_NONE, message_format=None):
