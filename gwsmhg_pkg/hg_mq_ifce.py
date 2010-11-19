b1c88417eeeafa2019c179851eaf3343b152ba8b
Peter Williams <pwil3058@gmail.com>
2010-11-19 11:03:51 +1000

    Mark directories "clean" if they only contain "clean" and "ignored" files
    Signed-off-by: Peter Williams <peter_ono@users.sourceforge.net>

--- gwsmhg_pkg/hg_mq_ifce.py
+++ gwsmhg_pkg/hg_mq_ifce.py
@@ -48,7 +48,7 @@ class ScmDir(fsdb.GenDir):
             self.status = FSTATUS_IGNORED
         elif self.status_set in [set([FSTATUS_NOT_TRACKED]), set([FSTATUS_NOT_TRACKED, FSTATUS_IGNORED])]:
             self.status = FSTATUS_NOT_TRACKED
-        elif self.status_set == set([FSTATUS_CLEAN]):
+        elif self.status_set in [set([FSTATUS_CLEAN]), set([FSTATUS_CLEAN, FSTATUS_IGNORED])]:
             self.status = FSTATUS_CLEAN
     def _is_hidden_dir(self, dkey):
         status = self.subdirs[dkey].status
